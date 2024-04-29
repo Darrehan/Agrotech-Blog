b1650f165b759d24000932158cc0adf08d9aea7f
Darrehan <darrehanrasool@gmail.com>
2024-04-29 10:39:17 +0530

    Dar Rehan Rasool
    sycomusiclover

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Agrotech Blog By Dar Rehan Rasool</h1>
+      <h1 className="text-3xl font-bold underline">HerbEsentia Blog</h1>
     </div>
   );
 }
